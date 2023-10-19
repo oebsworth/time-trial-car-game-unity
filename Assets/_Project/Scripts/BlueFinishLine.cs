ab8f353ca2adb82941767ac005bacc74f4757506
Oliver Ebsworth <ebswortholiver@gmail.com>
2023-10-19 22:14:09 +0100

    feat(game-level): Finished the game level design, also turned off the bluecar script when it hits the blue finish line.

--- Assets/_Project/Scripts/BlueFinishLine.cs
+++ Assets/_Project/Scripts/BlueFinishLine.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 public class BlueFinishLine : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI finishTimer;
+    [SerializeField] GameObject blueCar;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<BlueCar>() != null)
         {
             GameManager.Instance.state = GameManager.State.Finished;
+
+            blueCar.GetComponent<BlueCar>().enabled = false;
 
             finishTimer.gameObject.SetActive(true);
             finishTimer.text = "Finish: " + ((float)GameManager.Instance.currentRedTimer + (float)GameManager.Instance.currentBlueTimer).ToString("#.##");
