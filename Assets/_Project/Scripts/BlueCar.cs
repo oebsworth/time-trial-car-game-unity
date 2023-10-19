d25d3bdbf429e77231814ac845a7b5cd0ec54322
Oliver Ebsworth <ebswortholiver@gmail.com>
2023-10-19 19:51:00 +0100

    feat(blue-car): Implemented the blue car movement and components.

---
+++ Assets/_Project/Scripts/BlueCar.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueCar : MonoBehaviour
+{
+    // Movement
+    private Vector3 moveAxis;
+    private float moveSpeed = 4f;
+
+    // Turning
+    private Vector3 turnAxis;
+    private float turnSpeed = 80f;
+
+    private void Update()
+    {
+        moveCar();
+        turnCar();
+    }
+
+    private void moveCar()
+    {
+        moveAxis = new Vector3(0f, Input.GetAxis("Vertical"), 0f);
+        transform.Translate(moveAxis * moveSpeed * Time.deltaTime);
+    }
+
+    private void turnCar()
+    {
+        turnAxis = new Vector3(0f, 0f, -Input.GetAxis("Horizontal"));
+        transform.Rotate(turnAxis * turnSpeed * Time.deltaTime);
+    }
+}
