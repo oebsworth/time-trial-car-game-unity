0517acea87900b5842e302693fb85fc08c64d446
Oliver Ebsworth <ebswortholiver@gmail.com>
2023-10-19 21:32:20 +0100

    feat(finish-timer): Added the finish timer so that it shows a sum of the red and blue timers at the end of the game.

--- Assets/_Project/Scripts/BlueFinishLine.cs
+++ Assets/_Project/Scripts/BlueFinishLine.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BlueFinishLine : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI finishTimer;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<BlueCar>() != null)
         {
             GameManager.Instance.state = GameManager.State.Finished;
+
+            finishTimer.gameObject.SetActive(true);
+            finishTimer.text = ((float)GameManager.Instance.currentRedTimer + (float)GameManager.Instance.currentBlueTimer).ToString("#.##");
         }
     }
 }
