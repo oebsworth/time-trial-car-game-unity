536cefd7da5c992199ec4f5186df40259c8132f7
Oliver Ebsworth <ebswortholiver@gmail.com>
2023-10-27 12:09:55 +0100

    feat(tile-map): Adjusted the way the composite collider was being drawn for the road. Also changed how the triggers were being calculated.

--- Assets/_Main-Project/Scripts/Interactables/DirtTrigger.cs
+++ Assets/_Main-Project/Scripts/Interactables/DirtTrigger.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DirtTrigger : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<RedCar>() != null || collision.gameObject.GetComponent<BlueCar>() != null)
         {
